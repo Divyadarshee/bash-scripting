2a61b5b73ab93cd427bfe3cf7001882924c44c8d
Divyadarshee Das <divyadarsheedas@gmail.com>
2021-12-11 15:34:25 +0530

    Update first_sample_script.sh
    added expected output

--- first_sample_script.sh
+++ first_sample_script.sh
@@ -5,3 +5,7 @@ if grep "127.0.0.1" /etc/hosts; then
 else
   echo "ERROR: 127.0.0.1 not in /etc/hosts"
 fi
+# Standard OUTPUT:
+# 127.0.0.1       localhost
+# 127.0.0.1       kubernetes.docker.internal
+# Everything ok
